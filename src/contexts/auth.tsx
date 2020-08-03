f0b895988852bf7938a907e3423020663c06b702
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2020-08-02 22:36:54 -0300

    Salvando dados com AsyncStorage

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,4 +1,5 @@
 import React, {createContext} from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
 import * as auth from '../services/auth';
 
 interface AuthContextData {
@@ -13,10 +14,25 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 export const AuthProvider: React.FC = ({children}) => {
   const [user, setUser] = React.useState<object | null>(null);
 
+  React.useEffect(() => {
+    async function loadStoragedData() {
+      const storagedUser = await AsyncStorage.getItem('@RNAuth:user');
+      const storageToken = await AsyncStorage.getItem('@RNAuth:token');
+
+      if (storagedUser && storageToken) {
+        setUser(JSON.parse(storagedUser));
+      }
+    }
+    loadStoragedData();
+  }, []);
+
   async function signin() {
     const response = await auth.signIn();
 
     setUser(response.user);
+
+    await AsyncStorage.setItem('@RNAuth:user', JSON.stringify(response.user));
+    await AsyncStorage.setItem('@RNAuth:token', response.token);
   }
 
   function signOut() {
